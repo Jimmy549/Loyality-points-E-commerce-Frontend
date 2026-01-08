bc65c5ab987dc6c314a42e66cb0be57b550fe726
Jimmy549 <ehmitservices1@gmail.com>
2026-01-08 17:14:42 +0500

    fix: make onApply prop optional in Filters component

--- src/components/shop-page/filters/index.tsx
+++ src/components/shop-page/filters/index.tsx
@@ -6,7 +6,7 @@ import PriceSection from "@/components/shop-page/filters/PriceSection";
 import SizeSection from "@/components/shop-page/filters/SizeSection";
 import { Button } from "@/components/ui/button";
 
-const Filters = ({ onApply }: { onApply: (filters: any) => void }) => {
+const Filters = ({ onApply }: { onApply?: (filters: any) => void }) => {
   const [filters, setFilters] = useState({
     category: [],
     priceMin: 0,
@@ -37,7 +37,9 @@ const Filters = ({ onApply }: { onApply: (filters: any) => void }) => {
   };
 
   const handleApply = () => {
-    onApply(filters);
+    if (onApply) {
+      onApply(filters);
+    }
   };
 
   return (
